dbf154455ef56c1b2aeae3f41fdad5e1eb66366a
Karol Burczyński <s16556@pjwstk.edu.pl>
2020-03-01 16:06:39 +0100

    Getting http response

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Cwiczenia1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+            HttpResponseMessage result = await client.GetAsync("https://www.pja.edu.pl");
+            if(result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+            }
+            Console.WriteLine("Koniec");
         }
     }
 }
